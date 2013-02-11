edfc43e0383d82a75254224294b7136db84f6ffc
Tomomi Matsuda <tomomi@dio.jp>
2013-02-11 22:59:10 +0900

    Add Speakers page (Coming soon (?))

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ OedoRubykaigi03::Application.routes.draw do
   scope 'oedo03' do
     root to: 'top#show'
     get 'schedule' => 'top#schedule'
+    get 'speakers' => 'top#speakers'
     resources :talks, only: %w[index show]
   end
 end
