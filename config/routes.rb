746794e1f4ad4a9976c9fc09e5703cbb34b80309
Tomomi Matsuda <tomomi@dio.jp>
2013-02-11 22:54:05 +0900

    Add Schedule page and move timetable to the Schedule page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 OedoRubykaigi03::Application.routes.draw do
   scope 'oedo03' do
     root to: 'top#show'
+    get 'schedule' => 'top#schedule'
     resources :talks, only: %w[index show]
   end
 end
